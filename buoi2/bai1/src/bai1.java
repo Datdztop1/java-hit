73f7c44b62586784f2b9d1d5aab669fe4a8500b7
Datdztop1 <Nguyendat626644@gmail.com>
2025-07-20 18:57:44 +0700

    buoi2

---
+++ buoi2/bai1/src/bai1.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class bai1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Nhập tên: ");
+        String name = sc.nextLine();
+
+        System.out.print("Nhập tuổi: ");
+        int age = sc.nextInt();
+
+        System.out.print("Nhập chiều cao (mét): ");
+        double height = sc.nextDouble();
+
+        System.out.println("Xin chào " + name + ", bạn " + age + " tuổi và cao " + height + " mét.");
+    }
+}
